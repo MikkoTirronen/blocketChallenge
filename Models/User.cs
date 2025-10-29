91e3fb6f2321752155a09701d7e7a16d8653dcaa
MikkoTirronen <MikkoATirronen@gmail.com>
2025-10-29 14:50:40 +0100

    added User model

---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+namespace BlocketChallenge.Models;
+
+public class User
+{
+    public string Id { get; set; }
+    public string Username { get; set; }
+    public string PasswordHash { get; set; }
+    public string Email { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
