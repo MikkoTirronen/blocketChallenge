c329769634ef508080999d5f401e8cd5b337871e
Mikko Tirronen <mikkoatirronen@gmail.com>
2025-10-30 09:08:39 +0100

    added parameters

--- Repositories/IAdvertisementRepository.cs
+++ Repositories/IAdvertisementRepository.cs
@@ -5,8 +5,8 @@ namespace BlocketChallenge.Repositories;
 public interface IAdvertisementRepository
 {
     IEnumerable<Advertisement> GetAll();
-    Advertisement GetAdvertisementById(int id);
-    void Create();
-    void Update();
-    void Delete();
+    Advertisement? GetAdvertisementById(int id);
+    void Create(Advertisement ad);
+    void Update(Advertisement ad);
+    void Delete(int id);
 }
